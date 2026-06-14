58c11f6916ded7f1a27e2fdc88ae373a68fb5b0a
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-06-14 04:19:00 +0200

    feat: add runner

---
+++ src/main.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const vaxis = @import("vaxis");
+const vxfw = vaxis.vxfw;
+
+const proc = @import("proc.zig");
+
+const Model = @import("Model.zig");
+
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    const alloc = init.gpa;
+
+    var buffer: [1024]u8 = undefined;
+    var app: vxfw.App = try .init(io, alloc, init.environ_map, &buffer);
+    defer app.deinit();
+
+    run(init, &app) catch |err| {
+        std.debug.print("Error: {}\n", .{err});
+        return err;
+    };
+}
+
+fn run(init: std.process.Init, app: *vxfw.App) !void {
+    const alloc = init.gpa;
+    const model = try alloc.create(Model);
+    defer alloc.destroy(model);
+
+    const cpu_usage = try proc.readCpuUsage(init);
+    const ram_usage = try proc.readRamUsage(init);
+
+    model.* = .{
+        .cpu_usage = cpu_usage,
+        .ram_usage = ram_usage,
+        .button = .{
+            .label = "This is a button",
+            .onClick = onClick,
+            .userdata = model,
+        },
+    };
+
+    try app.run(model.widget(), .{});
+}
+
+fn onClick(maybe_ptr: ?*anyopaque, ctx: *vxfw.EventContext) anyerror!void {
+    const ptr = maybe_ptr orelse return;
+    const self: *Model = @ptrCast(@alignCast(ptr));
+    _ = self;
+    return ctx.consumeAndRedraw();
+}
